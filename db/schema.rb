37760b52ae5751f5f5f6766ebfbdb68e588e9818
dinh-hieu <dinhhieu1992@gmail.com>
2020-02-23 16:29:11 +0700

    create model micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_083312) do
+ActiveRecord::Schema.define(version: 2020_02_23_092639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_02_23_083312) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
