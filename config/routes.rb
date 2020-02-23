c8050a5af6d2cc51ddcc846c5a71bd51f090ac38
dinh-hieu <dinhhieu1992@gmail.com>
2020-02-23 22:58:29 +0700

    edit routes micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :microposts
   namespace :admin do
     resources :users
     resources :microposts
     resources :sessions, only:[:new, :create, :destroy]
   end
+  resources :microposts, only:[:index, :show]
   get '/login' => 'admin/sessions#new'
   get '/logout' => 'admin/sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
