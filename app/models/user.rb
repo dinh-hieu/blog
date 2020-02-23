bfa5487a08ad18c9bddb569bd42e66f2650f715c
dinh-hieu <dinhhieu1992@gmail.com>
2020-02-23 16:32:51 +0700

    add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :microposts
   validates :fullname, presence: true
   validates :email, presence: true, format: { with: /@/,message: "email is valid" }
   validates :password, presence: true
