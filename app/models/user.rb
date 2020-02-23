02c4025e9f5ee6500dcdd7a712e54fa9f49ee4c7
dinh-hieu <dinhhieu1992@gmail.com>
2020-02-23 15:48:37 +0700

    validate model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :fullname, presence: true
+  validates :email, presence: true, format: { with: /@/,message: "email is valid" }
+  validates :password, presence: true
 end
