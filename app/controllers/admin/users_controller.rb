372760030ee31f78f922a31ddcd692a41da69712
dinh-hieu <dinhhieu1992@gmail.com>
2020-02-23 17:50:05 +0700

    create controller admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,70 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /admin/users
+  # GET /admin/users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /admin/users/1
+  # GET /admin/users/1.json
+  def show
+  end
+
+  # GET /admin/users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /admin/users/1/edit
+  def edit
+  end
+
+  # POST /admin/users
+  # POST /admin/users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_user_path(@user), notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin/users/1
+  # PATCH/PUT /admin/users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_user_path(@user), notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /admin/users/1
+  # DELETE /admin/users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:fullname, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :microposts
+
   root 'home#index'
+  resources :microposts
+  namespace :admin do
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
