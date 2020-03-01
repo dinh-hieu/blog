7bddff7182eef8be167f5a684811b8e2632344de
dinh-hieu <dinhhieu1992@gmail.com>
2020-03-01 17:06:14 +0700

    add model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_092639) do
+ActiveRecord::Schema.define(version: 2020_03_01_094730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.string "title"
@@ -21,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_23_092639) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
