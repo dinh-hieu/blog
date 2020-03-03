90250e8f127f1f3eda251f4845935a12312fec82
dinh-hieu <dinhhieu1992@gmail.com>
2020-03-03 20:17:57 +0700

    edit migrate add column micropost

--- db/migrate/20200301101008_add_category_id_to_microposts.rb
+++ db/migrate/20200301101008_add_category_id_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToMicroposts < ActiveRecord::Migration[6.0]
   def change
-    add_column :microposts, :category_id, :interger
+    add_column :microposts, :category_id, :integer
   end
 end
