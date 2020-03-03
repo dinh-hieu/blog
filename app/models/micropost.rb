d05511f55bff21a640ced92a71998b3d044ca802
dinh-hieu <dinhhieu1992@gmail.com>
2020-03-03 21:11:09 +0700

    add action_text + upload image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   belongs_to :category
   scope :filter_by_category, -> (category_id) { where category_id: category_id }
+  has_rich_text :content
 end
