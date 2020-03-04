0a7f06370db9392c27f10f089442d17735b8f2b5
dinh-hieu <dinhhieu1992@gmail.com>
2020-03-04 20:44:32 +0700

    add url friendly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_135601) do
+ActiveRecord::Schema.define(version: 2020_03_04_133537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 2020_03_03_135601) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "microposts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -59,6 +70,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_135601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_microposts_on_slug", unique: true
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
